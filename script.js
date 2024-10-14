44bb5dd8e796979522d1e61ac371245c56372605
Iflaq khurshid <112414810+Iflaqbhat@users.noreply.github.com>
2024-10-14 11:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width,height){
+		this._width=width;
+		this._height=height;
+	}
+	get width(){
+		return this.width;
+	}
+	get height(){
+		return this.height;
+	}
+	getArea(){
+		let ans=(this._width)*(this._height);
+		return ans;
+	}
+}
 
-class Square extends Animal {}
+class Square extends Animal {
+	constructor(side){
+		super();
+		this.side=side;
+		
+	}
+	getPerimeter(){
+		return (this.side)*(this.side);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
